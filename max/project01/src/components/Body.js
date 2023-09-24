22b78ce3b305f8db742f1a02af4e3b4ea9aa2bcc
igorntwari <igorntwari28@gmail.com>
2023-09-24 14:22:27 +0200

    added new project

---
+++ max/project01/src/components/Body.js
@@ -0,0 +1,17 @@
+
+function Body() {
+    return (
+        <div>
+<ul id="concepts">
+<li className="concept">
+  <img src="TODO: IMAGE" alt="TODO: TITLE" />
+  <h2>TODO: TITLE</h2>
+  <p>TODO: DESCRIPTION</p>
+</li>
+</ul>
+
+        </div>
+    )
+}
+
+export default Body
